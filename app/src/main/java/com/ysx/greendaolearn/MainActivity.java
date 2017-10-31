7077e25b18b6145b32d22bf604f7e486b83fa0fa
develop_yang <develop_yang@yeah.net>
2017-10-31 14:52:37 +0800

    init the repository
    初始化仓库 HelloWorld
    
    Signed-off-by: develop_yang <develop_yang@yeah.net>

---
+++ app/src/main/java/com/ysx/greendaolearn/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ysx.greendaolearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author ysx
+ * @date 2017/10/31
+ * @description
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
